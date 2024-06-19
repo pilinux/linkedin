a83db074d21c31de3b857cc989a4c4d6007113de
pilinux <mahir.hasan@m.pilinux.me>
2024-06-19 08:29:48 +0200

    paging - get next/previous page

--- paging.go
+++ paging.go
@@ -1,5 +1,9 @@
 package linkedin
 
+import (
+	"strings"
+)
+
 // Paging struct for pagination
 type Paging struct {
 	Start int    `json:"start"`
@@ -13,4 +17,26 @@ type Link struct {
 	Type string `json:"type"` // application/json
 	Rel  string `json:"rel"`  // prev, next
 	Href string `json:"href"`
+}
+
+// GetNext returns the next page URL
+func (p *Paging) GetNext() (bool, string) {
+	for _, link := range p.Links {
+		if link.Rel == "next" {
+			// remove "/rest" from the URL
+			return true, strings.Replace(link.Href, "/rest", "", 1)
+		}
+	}
+	return false, ""
+}
+
+// GetPrev returns the previous page URL
+func (p *Paging) GetPrev() (bool, string) {
+	for _, link := range p.Links {
+		if link.Rel == "prev" {
+			// remove "/rest" from the URL
+			return true, strings.Replace(link.Href, "/rest", "", 1)
+		}
+	}
+	return false, ""
 }
